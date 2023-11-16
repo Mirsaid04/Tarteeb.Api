28c9f3820a03416439adb3a88d78ed904e5a05fb
Mirsaid04 <mirsaidd04@gmail.com>
2023-11-16 22:43:55 +0900

    Add project files.

---
+++ Tarteeb.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarteeb.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+
+        public ActionResult<string> GetHomeMessage() => "Tarteeb is running...";
+    }
+}
---
+++ Tarteeb.Api/Startup.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Tarteeb.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc(
+                    name:"v1",
+                    info: new OpenApiInfo { Title = "Tarteeb.Api", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(config => config.SwaggerEndpoint(
+                    url:"/swagger/v1/swagger.json",
+                    name: "Tarteeb.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+        }
+    }
+}
